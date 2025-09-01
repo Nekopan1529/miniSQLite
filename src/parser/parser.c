27f051fa1101c341f42bd3639572a5df14da3688
Nekopan1529 <nekocat189@gmail.com>
2025-09-01 20:18:02 +0900

    correctly defining parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,1 +1,13 @@
 #include "parser.h"
+
+ProcessResult process_input(InputBuffer* input_buffer, Statement* statement) {
+  if (strcmp(input_buffer->buf, "insert") == 0) {
+    statement->type = STATEMENT_INSERT;
+    return PROCESS_SUCCESS;
+  } else if (strcmp(input_buffer->buf, "select") == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PROCESS_SUCCESS;
+  }
+  printf("Unrecognized command '%s'\n", input_buffer->buf);
+  return PROCESS_UNKNOWN_STATEMENT;
+}
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -8,16 +8,6 @@
 
 typedef enum { PROCESS_SUCCESS, PROCESS_UNKNOWN_STATEMENT } ProcessResult;
 
-ProcessResult process_input(InputBuffer* input_buffer, Statement* statement) {
-  if (strcmp(input_buffer->buf, "insert") == 0) {
-    statement->type = STATEMENT_INSERT;
-    return PROCESS_SUCCESS;
-  } else if (strcmp(input_buffer->buf, "select") == 0) {
-    statement->type = STATEMENT_SELECT;
-    return PROCESS_SUCCESS;
-  }
-  printf("Unrecognized command '%s'\n", input_buffer->buf);
-  return PROCESS_UNKNOWN_STATEMENT;
-}
+ProcessResult process_input(InputBuffer* input_buffer, Statement* statement);
 
 #endif
